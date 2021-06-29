4904f2a9d21e4a170e3a3f9822009911d9fa00d2
FlyingPig17 <axelarturoc@gmail.com>
2021-06-28 21:56:34 -0500

    Added informative menu fragments

--- app/src/main/java/com/example/proyectoseguridadmujer/ui/informative/InformativeMenuFragment.java
+++ app/src/main/java/com/example/proyectoseguridadmujer/ui/informative/InformativeMenuFragment.java
@@ -1,19 +1,24 @@
 package com.example.proyectoseguridadmujer.ui.informative;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.proyectoseguridadmujer.R;
 
+import org.jetbrains.annotations.NotNull;
+
 public class InformativeMenuFragment extends Fragment {
 
-
+    ImageButton mImageButtonInstitution, mImageButtonTest;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,5 +27,32 @@ public class InformativeMenuFragment extends Fragment {
 
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Wiring up
+        mImageButtonInstitution = getView().findViewById(R.id.ImageButtonInstitution);
+        mImageButtonTest = getView().findViewById(R.id.ImageButtonTest);
+
+        mImageButtonInstitution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(), InstitutionListFragment.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        mImageButtonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(), TestFragment.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
     }
 }

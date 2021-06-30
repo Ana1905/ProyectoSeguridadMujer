254f01c494dfd40db55c2c51a2b81a656d062870
Ana1905 <66711242+Ana1905@users.noreply.github.com>
2021-06-29 22:59:39 -0500

    UPDATES

--- app/src/main/java/com/example/proyectoseguridadmujer/CreateAccountActivity.java
+++ app/src/main/java/com/example/proyectoseguridadmujer/CreateAccountActivity.java
@@ -224,8 +224,6 @@ public class CreateAccountActivity extends AppCompatActivity implements GoogleAp
 
             */
                                                     PutData putData = new PutData("http://seguridadmujer.com/app_movil/LoginRegister/signup.php", "POST", field, data);
-
-
                                                     if (putData.startPut()) {
                                                         if (putData.onComplete()) {
                                                             String result = putData.getResult();
